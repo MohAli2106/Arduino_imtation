e36597b9f4c9f67e7a17b52999216dac38d72c84
Moh77353749 <83034314+Moh77353749@users.noreply.github.com>
2025-01-21 13:23:46 +0200

    completed Arduino architecture

--- src/arduino.rs
+++ src/arduino.rs
@@ -1,5 +1,5 @@
 
-use std ::{collections::HashMap,time::Duration,thread}; //
+use std ::{collections::HashMap,time::Duration,thread} ;//
 
 
 pub const INPUT: u8 = 0;
@@ -8,22 +8,37 @@ pub const OUTPUT: u8 = 1;
 pub const LOW: u8 = 0;
 pub const HIGH: u8 = 1;
 
-
 pub struct Arduino{
     pins:HashMap<u8,u8>,
     dig_pins:HashMap<u8,u8>,
     anlg_pins:HashMap<u8,u16>,
 }
 
+
+
 impl Arduino{
     pub fn new() -> Self {
-        Arduino{
-            pins:HashMap::new(),
-            dig_pins:HashMap::new(),
-            anlg_pins:HashMap::new(),
+        let mut arduino = Arduino {
+            pins: HashMap::new(),
+            dig_pins: HashMap::new(),
+            anlg_pins: HashMap::new(),
+        };
 
+        // Initialize digital pins (0-13)
+        for pin in 0..14 {
+            arduino.pins.insert(pin, INPUT);
+            arduino.dig_pins.insert(pin, LOW);
         }
+
+        // Initialize analog pins (14-19)
+        for pin in 0..6 {
+            arduino.pins.insert(pin + 14, INPUT);
+            arduino.anlg_pins.insert(pin + 14, 0);
+        }
+
+        arduino
     }
+
     pub fn pinMode(&mut self,pin:u8,mode:u8){
         self.pins.insert(pin, mode);
     }
@@ -36,25 +51,41 @@ impl Arduino{
        if let Some(&mode)= self.pins.get(&pin){
             if mode==OUTPUT{
                 self.dig_pins.insert(pin,value);
-            }println!("Pin {} is not set as output",pin);
-        }println!("Pin {} is not set",pin);
+            }
+            else{
+                println!("Pin {} is not set as output",pin);
+                ()
+            }
+        }
+        else{
+            println!("Pin {} is not set",pin);
+            ()
+        }
     }
+
     pub fn anlg_write(&mut self, pin:u8,value:u16){
         if let Some(&mode)=self.pins.get(&pin){
             if mode==OUTPUT{
                 self.anlg_pins.insert(pin,value);
                     
-            }println!("Pin {} is not set as output",pin);
+            }
+            else {
+                println!("Pin {} is not set as output",pin);
+                ()
+            }
 
-        }println!("Pin {} isnot set",pin)
+        }
+        else {
+            println!("Pin {} isnot set",pin);
+            ()
+        }
 
     }
     pub fn anlg_read(&self,pin:u8)->Option<u16>{
         self.anlg_pins.get(&pin).copied()
-    }
+    }       
 
-    pub fn dely(ms:u64){
+    pub fn dely(&self, ms: u64) {
         thread::sleep(Duration::from_millis(ms));
     }
-
-}
+}    
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-//mod arduino;
-mod Arduino;
-
+mod arduino;
+use arduino::{OUTPUT, INPUT, HIGH,Arduino};
 
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut arduino = Arduino::new();
+
+    // Set pin 10 as OUTPUT and write HIGH
+    arduino.pinMode(10, OUTPUT);
+    arduino.dig_write(10, HIGH);
+
+    // Set pin 10 as INPUT and read from it
+    arduino.pinMode(10, INPUT);
+    if let Some(value) = arduino.dig_read(10) {
+        println!("Value of pin 10 is {}", value);
+    }
+
+    // Set pin 11 as INPUT and read from it
+    arduino.pinMode(11, INPUT);
+    if let Some(value) = arduino.dig_read(11) {
+        println!("Value of pin 11 is {}", value);
+    }
+
+    
+    arduino.dely(1000);
 }
